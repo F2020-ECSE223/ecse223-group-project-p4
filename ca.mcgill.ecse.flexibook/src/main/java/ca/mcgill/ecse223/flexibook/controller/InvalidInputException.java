108f0e3fe22ba62754f92bdcaef26a0351b6c275
snehas99 <70704577+snehas99@users.noreply.github.com>
2020-10-23 20:35:19 -0400

    Delete InvalidInputException.java

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse223/flexibook/controller/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package ca.mcgill.ecse223.flexibook.controller;
-
-public class InvalidInputException extends Exception{
-
-	public InvalidInputException(String errorMessage) {
-		super(errorMessage);
-	}
-
-}
