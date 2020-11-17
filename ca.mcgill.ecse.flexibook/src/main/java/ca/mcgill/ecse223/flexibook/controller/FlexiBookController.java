93d7ce74a562456f16bcc6a2c1c9f7e542c98ec0
Sneha Singh <sneha.singh@mail.mcgill.ca>
2020-11-16 21:31:58 -0500

    started on Update Service

--- ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse223/flexibook/controller/FlexiBookController.java
+++ ca.mcgill.ecse.flexibook/src/main/java/ca/mcgill/ecse223/flexibook/controller/FlexiBookController.java
@@ -1290,7 +1290,8 @@ public class FlexiBookController {
 	/**
 	 * AddService Feature Implementation
 	 * 
-	 * @author Sneha Singh {@summary} Adds service with specified parameters to
+	 * @author Sneha Singh 
+	 * {@summary} Adds service with specified parameters to
 	 *         FlexiBook if all criteria is satisfied
 	 * @param name
 	 * @param flexiBook
